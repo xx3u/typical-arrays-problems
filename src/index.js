f15af01624ae9ad9cda66ef58aa88bee805cef7a
YK <yuliya.khv@gmail.com>
2021-02-15 16:37:27 +0600

    feat: add min, max, avg calculation for array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == 0 || array === undefined) {
+    return 0
+  } else {
+    return array.reduce((p, v) => {
+      return ( p < v ? p : v)
+    }, 0);
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == 0 || array === undefined) {
+    return 0
+  } else {
+    return array.reduce((p, v) => {
+      return ( p > v ? p : v)
+    }, 0)
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == 0 || array === undefined) {
+    return 0
+  } else { 
+    const sum = array.reduce((a, b) => a + b, 0);
+    const avg = (sum / array.length) || 0;
+    return avg;
+  }
 }
